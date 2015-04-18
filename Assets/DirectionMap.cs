86be8e3a640fb1e58a4b95a2fd2b61ce58a37361
Martin Vilcans <martin@librador.com>
2015-04-18 18:46:30 +0200

    Add DirectionMap component

---
+++ Assets/DirectionMap.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionMap : MonoBehaviour {
+
+    public TileMap map;
+    public int targetColumn;
+    public int targetRow;
+
+    // Costs to move to this object from every position on the map
+    private float[] costs;
+
+    void Start() {
+        Debug.Log("Starting " + this.name);
+        costs = new float[map.width * map.height];
+        for(var i = 0; i < map.NumberOfIndices; ++i) {
+            costs[i] = Mathf.Infinity;
+        }
+        UpdateMap();
+        Print();
+    }
+
+    public void UpdateMap() {
+        UpdateRecursively(targetColumn, targetRow);
+    }
+
+    private void UpdateRecursively(int col, int row, float totalCost=0, int recursionDepth=0) {
+        Debug.Log("Update map at col " + col + " row " + row + " recursion depth " + recursionDepth);
+        if(recursionDepth > 40 /*System.Math.Max(map.width, map.height*/) {
+            Debug.LogWarning("Max recursion reached!");
+            Print();
+            throw new System.ApplicationException("Max recursion");
+        }
+        costs[map.GetIndex(col, row)] = totalCost;
+        foreach(TileMap.Direction dir in System.Enum.GetValues(typeof(TileMap.Direction))) {
+            int newCol = col;
+            int newRow = row;
+            map.Walk(ref newCol, ref newRow, dir);
+            if(!map.IsInBounds(newCol, newRow)) {
+                Debug.Log("Out of bounds: " + newCol + " " + newRow);
+                continue;
+            }
+            int index = map.GetIndex(newCol, newRow);
+            float newCost = totalCost + map.GetCost(map.GetTile(newCol, newRow));
+            if(newCost < costs[index]) {
+                Debug.Log("Cost for " + newCol + " " + newRow + " is " + newCost + " - better than " + costs[index]);
+                UpdateRecursively(newCol, newRow, newCost, recursionDepth + 1);
+            }
+            else {
+                Debug.Log("Cost for " + newCol + " " + newRow + " is " + newCost + " - not better than " + costs[index]);
+            }
+        }
+    }
+
+    public void Print() {
+        for(int row = 0; row < map.height; ++row) {
+            string s = string.Format("{0,-3}", row) + ": ";
+            for(int col = 0; col < map.width; ++col) {
+                int index = map.GetIndex(col, row);
+                float w = costs[index];
+                string f = (w == Mathf.Infinity) ? "-    " : string.Format("{0,-5:F1}", w);
+                s += f;
+                s += " ";
+            }
+            Debug.Log(s);
+        }
+    }
+}
--- Assets/TileMap.cs
+++ Assets/TileMap.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 
 public class TileMap : MonoBehaviour {
 
-	public GameObject homePrefab;
-	public GameObject schoolPrefab;
-	public GameObject workPrefab;
-	public GameObject terrorPrefab;
+    public enum Direction {
+        Left, Right, Up, Down
+    };
 
-	private MapData mapData = new MapData();
-	private int width = 30;
-	private int height = 20;
-	private byte[] tileData = {48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,56,57,24,25,26,57,24,25,26,57,24,25,26,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,48,49,32,33,34,49,32,33,34,49,32,33,34,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,56,57,40,41,42,57,40,41,42,57,40,41,42,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,3,4,5,57,56,57,56,57,56,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,11,12,13,49,48,49,48,49,48,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,19,20,21,57,56,57,56,57,56,48,49,48,49,48,49,48,49,0,1,2,49,48,49,48,49,48,49,48,49,48,49,50,49,49,48,49,48,49,48,56,57,56,57,56,57,56,57,8,9,10,50,50,50,50,50,50,50,50,50,50,50,50,57,57,56,57,56,57,56,48,49,48,49,48,49,48,49,16,17,18,49,48,49,48,49,48,49,48,49,48,49,49,48,49,48,49,48,49,48,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,57,56,57,56,57,56,57,56,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,49,48,49,48,49,48,49,48,56,57,27,28,29,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,57,56,57,56,57,56,57,56,48,49,35,36,37,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,49,48,49,48,49,48,49,48,56,57,43,44,45,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,57,56,57,56,57,56,57,56,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,48,49,49,48,49,48,49,48,49,48,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,56,57,57,56,57,56,57,56,57,56};
+    public GameObject homePrefab;
+    public GameObject schoolPrefab;
+    public GameObject workPrefab;
+    public GameObject terrorPrefab;
 
-	void Start() {
-		this.width = mapData.width;
-		this.height = mapData.height;
-		this.tileData = mapData.tileData;
+    public float groundWeight = 1.0f;
+    public float dirtWeight = 1.0f;
+
+    private MapData mapData = new MapData();
+    public int width = 30;
+    public int height = 20;
+    private byte[] tileData;
+
+    void Start() {
+        this.width = mapData.width;
+        this.height = mapData.height;
+        this.tileData = mapData.tileData;
         CreateObjects();
     }
 
@@ -30,35 +37,94 @@ public class TileMap : MonoBehaviour {
                     continue;
                 }
                 obj.transform.position = RowColToWorld(row, col);
-				Debug.Log("Instantiated " + obj.name + " at col " + col + " row " + row + " " + obj.transform.position);
+                DirectionMap directionMap = obj.GetComponent<DirectionMap>();
+                if(directionMap != null) {
+                    directionMap.map = this;
+                    directionMap.targetColumn = col;
+                    directionMap.targetRow = row;
+                }
+                Debug.Log("Instantiated " + obj.name + " at col " + col + " row " + row + " " + obj.transform.position);
             }
         }
     }
 
     GameObject CreateObject(byte tileCode) {
-		if(tileCode == 0) {
-			GameObject obj = (GameObject)Object.Instantiate(homePrefab);
-			obj.name = "Home";
-			return obj;
-		}
-		if(tileCode == 3) {
-			GameObject obj = (GameObject)Object.Instantiate(schoolPrefab);
-			obj.name = "School";
-			return obj;
-		}
-		if(tileCode == 24) {
-			GameObject obj = (GameObject)Object.Instantiate(workPrefab);
-			obj.name = "Work";
-			return obj;
-		}
-		if(tileCode == 27) {
-			GameObject obj = (GameObject)Object.Instantiate(terrorPrefab);
-			return obj;
-		}
-		return null;
+        if(tileCode == 0) {
+            GameObject obj = (GameObject)Object.Instantiate(homePrefab);
+            obj.name = "Home";
+            return obj;
+        }
+        if(tileCode == 3) {
+            GameObject obj = (GameObject)Object.Instantiate(schoolPrefab);
+            obj.name = "School";
+            return obj;
+        }
+        if(tileCode == 24) {
+            GameObject obj = (GameObject)Object.Instantiate(workPrefab);
+            obj.name = "Work";
+            return obj;
+        }
+        if(tileCode == 27) {
+            GameObject obj = (GameObject)Object.Instantiate(terrorPrefab);
+            return obj;
+        }
+        return null;
     }
 
     public Vector3 RowColToWorld(int row, int col) {
         return new Vector3(col - width * .5f, 0, height * .5f - row);
+    }
+
+    public int NumberOfIndices {
+        get { return this.width * this.height; }
+    }
+
+    public bool IsInBounds(int col, int row) {
+        return col >= 0 && col < width && row >= 0 && row < height;
+    }
+
+    public byte GetTile(int col, int row) {
+        return tileData[GetIndex(col, row)];
+    }
+
+    public float GetCost(byte tileType) {
+        switch(tileType) {
+        case 48:
+        case 49:
+        case 56:
+        case 57:
+            return groundWeight;
+        default:
+			return Mathf.Infinity;
+        }
+    }
+
+    public int GetIndex(int col, int row) {
+        if(col < 0 || col >= width) {
+            throw new System.ApplicationException("Column out of range: " + col);
+        }
+        if(row < 0 || row >= height) {
+            throw new System.ApplicationException("Row out of range: " + row);
+        }
+        return col + row * width;
+    }
+
+    public void Walk(ref int col, ref int row, Direction dir) {
+        switch(dir) {
+        case Direction.Right:
+            ++col;
+            break;
+        case Direction.Left:
+            --col;
+            break;
+        case Direction.Up:
+            --row;
+            break;
+        case Direction.Down:
+            ++row;
+            break;
+        default:
+            throw new System.ApplicationException("Unknown Direction");
+        }
     }
 }
