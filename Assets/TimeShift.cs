c6b3553e54e1638f1ba645228b7fe6ac4a8ddab7
Martin Vilcans <martin@librador.com>
2015-04-19 21:17:46 +0200

    Time shift with 0 key

---
+++ Assets/TimeShift.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeShift : MonoBehaviour {
+
+    public bool fast = false;
+
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.Alpha0)) {
+            this.fast = !this.fast;
+            Time.timeScale = fast ? 5 : 1;
+        }
+	}
+}
