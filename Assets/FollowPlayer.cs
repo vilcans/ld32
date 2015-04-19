550d43b5735dfc2b54f89c052bf3c2f7e632d839
Martin Vilcans <martin@librador.com>
2015-04-19 22:55:49 +0200

    Follow player with camera

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;
+    
+    void Update() {
+        Vector3 my = this.gameObject.transform.position;
+        this.transform.position = new Vector3(target.position.x, my.y, my.z);
+
+        /* = new Vector3(
+            target.x,
+            my.x,
+            my.y
+        );*/
+    }
+}
