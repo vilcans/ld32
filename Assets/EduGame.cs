198b59a91a60d8a96627d29d78f57b72e8e4158e
Martin Vilcans <martin@librador.com>
2015-04-20 02:00:46 +0200

    Show lose image on game over

--- Assets/EduGame.cs
+++ Assets/EduGame.cs
@@ -6,8 +6,12 @@ public class EduGame : MonoBehaviour {
     public enum State {
         InGame,
         GameOver,
+        Win
     };
     public State state = State.InGame;
+
+    public Texture loseTexture;
+    public Texture winTexture;
 
     public void EnterGameOver() {
         if(state != State.InGame) {
@@ -15,5 +19,24 @@ public class EduGame : MonoBehaviour {
         }
         Debug.Log("Entering game over state");
         state = State.GameOver;
+    }
+
+    public void OnGUI() {
+        if(state == State.GameOver) {
+            GUI.DrawTexture(
+                new Rect(0, 0, Screen.width, Screen.height),
+                loseTexture,
+                ScaleMode.ScaleToFit,
+                alphaBlend: true
+            );
+        }
+        else if(state == State.Win) {
+            GUI.DrawTexture(
+                new Rect(0, 0, Screen.width, Screen.height),
+                loseTexture,
+                ScaleMode.ScaleToFit,
+                alphaBlend: true
+            );
+        }
     }
 }
